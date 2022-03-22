742cc2866ff20f441afdb92c3ed8b07d200442a1
femketrampo <76632910+femketrampo@users.noreply.github.com>
2022-03-22 09:51:26 +0100

    Add files via upload

---
+++ Opdracht-1-GETSET/Opdracht-1-GETSET/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Opdracht_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<leerling> Leerlingen = new List<leerling>();
+            int keuze;
+            string antwoord = "";
+
+            do
+            {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Blue;
+
+                Console.WriteLine("1) Leerling toevoegen");
+                Console.WriteLine("2) Gegevens aanpassen");
+                Console.WriteLine("3) Gegevens bekijken");
+                Console.WriteLine("4) Exit");
+                Console.Write("\n Keuze: ");
+                keuze = Convert.ToInt32(Console.ReadLine());
+
+                Console.Clear();
+
+                if (keuze <= 4)
+                {
+                    switch (keuze)
+                    {
+                        case 1:
+                            {
+                                do
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Green;
+                                    Leerlingen.Add(Toevoegen(Leerlingen));
+
+                                    Console.Write("Wilt u nog een leerling toevoegen?(J/N): ");
+                                    antwoord = Console.ReadLine();
+                                } while (antwoord.ToUpper() == "J");
+                            }
+                            break;
+
+                        case 2:
+                            {
+                                Console.ForegroundColor = ConsoleColor.Cyan;
+                                Aanpassen(Leerlingen);
+                            }
+                            break;
+
+                        case 3:
+                            {
+                                Weergeven(Leerlingen);
+
+                                Console.ReadLine();
+                            }
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+
+                    Console.WriteLine("Dit is geen geldige optie!");
+                    Console.ReadLine();
+                }
+            } while (keuze != 4);
+            Console.ReadLine();
+        }
+
+        private static leerling Toevoegen(List<leerling> Leerlingen)
+        {
+            Console.Clear();
+            leerling Leerling = new leerling();
+
+            Console.Write("Geef de voornaam van de leerling in: ");
+            Leerling.Voornaam = Console.ReadLine();
+            Console.Write("Geef de achternaam van de leerling in: ");
+            Leerling.Achternaam = Console.ReadLine();
+            Console.Write("Geef de klas van de leerling in: ");
+            Leerling.Klas = Console.ReadLine();
+
+            return (Leerling);
+        }
+
+        private static void Aanpassen(List<leerling> Leerlingen)
+        {
+            Console.Clear();
+            leerling Leerling = new leerling();
+            int Nummer;
+
+            Console.WriteLine("Er zijn {0} leerlingen ingegeven", Leerlingen.Count);
+            Console.Write("Welke leerling wilt u aanpassen? ");
+            Nummer = Convert.ToInt32(Console.ReadLine());
+
+            Console.Clear();
+
+            Console.Write("Geef de voornaam van de leerling in: ");
+            Leerling.Voornaam = Console.ReadLine();
+            Console.Write("Geef de achternaam van de leerling in: ");
+            Leerling.Achternaam = Console.ReadLine();
+            Console.Write("Geef de klas van de leerling in: ");
+            Leerling.Klas = Console.ReadLine();
+
+            Leerlingen.RemoveAt(Nummer - 1);
+            Leerlingen.Insert(Nummer - 1, Leerling);
+        }
+
+        private static void Weergeven(List<leerling> Leerlingen)
+        {
+            Console.Clear();
+
+
+            if (Leerlingen.Count == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+
+                Console.WriteLine("Er zijn nog geen leerlingen ingegeven!");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+
+                int teller = 1;
+                foreach (leerling Leerling in Leerlingen)
+                {
+                    Console.WriteLine("Leerling {0}:", teller);
+                    Console.WriteLine("naam: " + Leerling.Voornaam + " " + Leerling.Achternaam);
+                    Console.WriteLine("klas: {0}", Leerling.Klas);
+                    Console.WriteLine();
+                    teller++;
+
+                    //REMEMBER!!
+                    //Optie 2: (check class)
+
+                    //Console.WriteLine(Leerling);
+                    //Leerling.Teller++;
+                }
+            }
+        }
+    }
+}
---
+++ Opdracht-1-GETSET/Opdracht-1-GETSET/leerling.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Opdracht_1
+{
+    class leerling
+    {
+        private string voornaam;
+        private string achternaam;
+        private string klas;
+        public int Teller = 1;
+
+        public string Voornaam
+        {
+            get { return voornaam; }
+            set { voornaam = value; }
+        }
+
+        public string Achternaam
+        {
+            get { return achternaam; }
+            set { achternaam = value; }
+        }
+
+        public string Klas
+        {
+            get { return klas; }
+            set { klas = value; }
+        }
+
+        public leerling()
+        {
+            voornaam = "leeg";
+            achternaam = "leeg";
+            klas = "leeg";
+        }
+
+        public leerling(string Voornaam, string Achternaam, string Klas)
+        {
+            voornaam = Voornaam;
+            achternaam = Achternaam;
+            klas = Klas;
+        }
+
+        //REMEMBER!!
+        //Optie 2: (check main)
+
+        //public override string ToString()
+        //{
+        //    return "Leerling " + this.Teller + ":" + "\n" + "naam: " + this.voornaam + " " + this.achternaam + "\n" + "klas: " + this.klas + "\n";
+        //}
+    }
+}
